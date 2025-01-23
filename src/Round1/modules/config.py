ba285b4e0d0bbb20f20de4c9ae66af49e4f60f13
MuhammadAhmadMirza <167323764+MuhammadAhmadMirza@users.noreply.github.com>
2025-01-24 01:52:44 +0500

    made code into single notebook and finally ran the model successfully

--- src/Round1/modules/config.py
+++
@@ -1,4 +0,0 @@
-dataset_path = 'src/Round1/data/dataset'
-raw_dataset_path = 'src/Round1/data/raw'
-training_dataset_path = 'src/Round1/data/dataset/training'
-testing_dataset_path = 'src/Round1/data/dataset/testing'
